e68682b68c9d5f860b34fd1b73183b8b752a5b5b
VaheSargsyan2005 <w33bv.gl@gmail.com>
2025-11-25 13:03:31 +0400

    Updates

--- _nuxt/Cp4OByBl.js
+++
@@ -1,1 +0,0 @@
-function o(l,s,c){const e=Math.max(l,s),f=Math.max(20,Math.round(e/4));let t;e<=100?t=.3:e<=500?t=.4:t=.5;let a;return e<=100?a=10:e<=500?a=15:a=20,{h:f,blur:t,q:a,f:c??"webp"}}export{o as c};
